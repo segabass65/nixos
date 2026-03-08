bb8bd7d47d67891bd15b7524791e4333f035f9b6
segabass65 <andreypiskunov2008@gmail.com>
2026-03-09 01:30:40 +0300

    Сonfigure Librewolf

---
+++ configs/librewolf/user.js
@@ -0,0 +1,4 @@
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("reader.parse-on-load.enabled", false);
+user_pref("webgl.enable-debug-renderer-info", false);
